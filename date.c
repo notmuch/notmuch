4f9aa77d8005af3e4d0e20d841b3ad48ede64929
Carl Worth <cworth@cworth.org>
2009-10-19 13:02:17 -0700

    date.c: Convert from LGPL-2+ to GPL-3+
    As authorized by LGPL-2 term (3).

--- date.c
+++ date.c
@@ -1,23 +1,27 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-/*  GMime
- *  Copyright (C) 2000-2009 Jeffrey Stedfast
+/* date.c - Date-parsing utility for the notmuch mail system.
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2.1
- *  of the License, or (at your option) any later version.
+ *  Copyright © 2000-2009 Jeffrey Stedfast
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free
- *  Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
- *  02110-1301, USA.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/ 
  */
 
+/* This code was originally written by from Jeffrey Stedfast
+ * as part of his GMime library (http://spruce.sourceforge.net/gmime/)
+ *
+ * Carl Worth <cworth@cworth.org> imported it into notmuch and removed
+ * some glib-isms.
+ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
