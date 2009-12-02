1466e249e741e3ef907e8c48c2861742b676e255
Carl Worth <cworth@cworth.org>
2009-12-01 16:46:21 -0800

    getdelim: Silence a (bogus) compiler warning.
    Some compilers complain that result might be used uninitialized in
    this function. I believe such compilers simply aren't looking hard
    enough, but it's easy enough to silence them.

--- compat/getdelim.c
+++ compat/getdelim.c
@@ -54,7 +54,7 @@
 ssize_t
 getdelim (char **lineptr, size_t *n, int delimiter, FILE *fp)
 {
-  ssize_t result;
+  ssize_t result = -1;
   size_t cur_len = 0;
 
   if (lineptr == NULL || n == NULL || fp == NULL)
