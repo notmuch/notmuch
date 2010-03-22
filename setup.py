c3239d3eb47eeaa1ed96a3169c0effc4e68185de
Sebastian Spaeth <sebastian@sspaeth.de>
2010-03-22 10:48:03 +0100

    setup.py and MANIFEST.in for the 0.1 release. Unfortunately post-version tag.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cnotmuch',
+      version='0.1',
+      description='Python binding og the notmuch mail search and indexing library.',
+      author='Sebastian Spaeth',
+      author_email='Sebastian@SSpaeth.de',
+      url='http://bitbucket.org/spaetz/cnotmuch/',
+      download_url='http://bitbucket.org/spaetz/cnotmuch/get/v0.1.tar.gz',
+      packages=['cnotmuch'],
+      keywords = ["library", "email"],
+      long_description="""The cnotmuch  module provides an interface to the `notmuch <http://notmuchmail.org>`_ functionality, directly interfacing with a shared notmuch library. Notmuch provides a maildatabase that allows for extremely quick searching and filtering of your email according to various criteria.
+
+The documentation for the latest cnotmuch release can be `viewed online <http://spaetz.bitbucket.org/>`_.
+
+The classes notmuch.Database, notmuch.Query  provide most of the core functionality, returning notmuch.Messages and notmuch.Tags.
+""",
+      classifiers=['Development Status :: 2 - Pre-Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Communications :: Email',
+                   'Topic :: Software Development :: Libraries'
+                   ],
+      platforms='',
+      license='http://www.gnu.org/licenses/gpl-3.0.txt',
+     )
