2a5884633c341660e5cc4c841dfe6898e47b9a60
Sebastian Spaeth <sebastian@sspaeth.de>
2010-04-07 11:43:29 +0200

    Extent pypi documentation. Set version to 0.2.2

--- setup.py
+++ setup.py
@@ -11,11 +11,33 @@ setup(name='cnotmuch',
       download_url='http://bitbucket.org/spaetz/cnotmuch/get/v'+__VERSION__+'.tar.gz',
       packages=['cnotmuch'],
       keywords = ["library", "email"],
-      long_description="""The cnotmuch  module provides an interface to the `notmuch <http://notmuchmail.org>`_ functionality, directly interfacing with a shared notmuch library. Notmuch provides a maildatabase that allows for extremely quick searching and filtering of your email according to various criteria.
+      long_description="""Overview
+==============
+
+The cnotmuch  module provides an interface to the `notmuch <http://notmuchmail.org>`_ functionality, directly interfacing with a shared notmuch library. Notmuch provides a maildatabase that allows for extremely quick searching and filtering of your email according to various criteria.
 
 The documentation for the latest cnotmuch release can be `viewed online <http://packages.python.org/cnotmuch>`_.
 
 The classes notmuch.Database, notmuch.Query  provide most of the core functionality, returning notmuch.Messages and notmuch.Tags.
+
+Installation and Deinstallation
+-------------------------------
+
+cnotmuch is packaged on http://pypi.python.org. This means you can do
+"easy_install cnotmuch" (or using pip) on your linux box and it will
+get installed into:
+
+/usr/local/lib/python2.x/dist-packages/
+
+For uninstalling, you will need to remove the "cnotmuch-0.1-py2.x.egg"
+directory and delete one entry refering to cnotmuch in the
+"easy-install.pth" file in that directory. There should be no trace
+left of cnotmuch then.
+
+Requirements
+------------
+
+You need to have notmuch installed (or rather libnotmuch.so.1). The release version 0.1 should work fine. Also, cnotmuch makes use of the ctypes library, and has only been tested with python 2.5. It will not work on earlier python versions.
 """,
       classifiers=['Development Status :: 2 - Pre-Alpha',
                    'Intended Audience :: Developers',
