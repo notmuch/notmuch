bab77c90844eede014fcf79414bab38add83a3b9
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2010-11-24 00:45:43 -0500

    Add a .dir-locals.el file for the benefit of emacs users
    This file causes emacs to automatically set the correct style
    variables to control indentation, etc. One more thing to make it
    easier for everyone to collaborate with a consistent coding style.
    
    Signed-off-by: Jameson Rollins <jrollins@finestructure.net>

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+; emacs local configuration settings for notmuch source
+; surmised by dkg on 2010-11-23 13:43:18-0500
+
+((c-mode . ((indent-tabs-mode . t)
+            (tab-width . 8)
+            (c-basic-offset . 4)
+            (c-file-style . "linux"))))
