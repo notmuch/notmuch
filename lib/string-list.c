b599bbe6721f6320c732ec7187f798ce60781887
Austin Clements <amdragon@mit.edu>
2011-03-21 02:33:55 -0400

    Fixup string list author

--- lib/string-list.c
+++ lib/string-list.c
@@ -16,6 +16,7 @@
  * along with this program.  If not, see http://www.gnu.org/licenses/ .
  *
  * Author: Carl Worth <cworth@cworth.org>
+ *         Austin Clements <aclements@csail.mit.edu>
  */
 
 #include "notmuch-private.h"
