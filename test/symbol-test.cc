0da10aa1bc021bdf7fb43c818db1d59f28f30ecd
Amadeusz Żołnowski <aidecoe@aidecoe.name>
2011-11-25 16:34:48 +0100

    Whitespaces cleanup.

--- test/symbol-test.cc
+++ test/symbol-test.cc
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <xapian.h>
 #include <notmuch.h>
-int main (){
 
-  (void)notmuch_database_open ("fakedb",
-				     NOTMUCH_DATABASE_MODE_READ_ONLY);
 
-  try{
-    (void)new Xapian::WritableDatabase ("./nonexistant",					Xapian::DB_OPEN);
+int main() {
+  (void) notmuch_database_open("fakedb", NOTMUCH_DATABASE_MODE_READ_ONLY);
+
+  try {
+    (void) new Xapian::WritableDatabase("./nonexistant", Xapian::DB_OPEN);
   } catch (const Xapian::Error &error) {
-    printf("caught %s\n",error.get_msg().c_str());
+    printf("caught %s\n", error.get_msg().c_str());
     return 0;
   }
+
   return 1;
 }
