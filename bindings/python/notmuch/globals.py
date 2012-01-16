7ddd849015759a329bf8fef8c8b5a93359408962
Justus Winter <4winter@informatik.uni-hamburg.de>
2012-01-16 13:39:41 +0100

    py3k: add a specialized version of _str for python3
    All strings are unicode strings in python 3 and the basestring and
    unicode types are removed hence the need for a specialized version.
    
    Signed-off-by: Justus Winter <4winter@informatik.uni-hamburg.de>

--- bindings/python/notmuch/globals.py
+++ bindings/python/notmuch/globals.py
@@ -31,10 +31,32 @@ if sys.version_info[0] == 2:
     class Python3StringMixIn(object):
         def __str__(self):
             return unicode(self).encode('utf-8')
+
+
+    def _str(value):
+        """Ensure a nicely utf-8 encoded string to pass to libnotmuch
+
+        C++ code expects strings to be well formatted and
+        unicode strings to have no null bytes."""
+        if not isinstance(value, basestring):
+            raise TypeError("Expected str or unicode, got %s" % type(value))
+        if isinstance(value, unicode):
+            return value.encode('UTF-8')
+        return value
 else:
     class Python3StringMixIn(object):
         def __str__(self):
             return self.__unicode__()
+
+
+    def _str(value):
+        """Ensure a nicely utf-8 encoded string to pass to libnotmuch
+
+        C++ code expects strings to be well formatted and
+        unicode strings to have no null bytes."""
+        if not isinstance(value, str):
+            raise TypeError("Expected str, got %s" % type(value))
+        return value.encode('UTF-8')
 
 
 class Enum(object):
@@ -200,18 +222,6 @@ class NotInitializedError(NotmuchError):
     been exhausted. You can test for NotmuchError with .status =
     STATUS.NOT_INITIALIZED"""
     status = STATUS.NOT_INITIALIZED
-
-
-def _str(value):
-    """Ensure a nicely utf-8 encoded string to pass to libnotmuch
-
-    C++ code expects strings to be well formatted and
-    unicode strings to have no null bytes."""
-    if not isinstance(value, basestring):
-        raise TypeError("Expected str or unicode, got %s" % str(type(value)))
-    if isinstance(value, unicode):
-        return value.encode('UTF-8')
-    return value
 
 
 class NotmuchDatabaseS(Structure):
