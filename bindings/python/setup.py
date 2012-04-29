3ff39766d460446a1cd1f6731511baca9584ffa2
Justus Winter <4winter@informatik.uni-hamburg.de>
2012-04-29 16:26:36 +0200

    python: update the long description in setup.py
    Signed-off-by: Justus Winter <4winter@informatik.uni-hamburg.de>

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -20,28 +20,16 @@ setup(name='notmuch',
       packages=['notmuch'],
       keywords = ["library", "email"],
       long_description="""Overview
-==============
+========
 
-The notmuch  module provides an interface to the `notmuch <http://notmuchmail.org>`_ functionality, directly interfacing with a shared notmuch library. Notmuch provides a maildatabase that allows for extremely quick searching and filtering of your email according to various criteria.
+The notmuch module provides an interface to the `notmuch
+<http://notmuchmail.org>`_ functionality, directly interfacing with a
+shared notmuch library. Notmuch provides a maildatabase that allows
+for extremely quick searching and filtering of your email according to
+various criteria.
 
-The documentation for the latest cnotmuch release can be `viewed online <http://packages.python.org/notmuch>`_.
-
-The classes notmuch.Database, notmuch.Query  provide most of the core functionality, returning notmuch.Messages and notmuch.Tags.
-
-Installation and Deinstallation
--------------------------------
-
-notmuch is included in the upstream notmuch source repository and it is
-packaged on http://pypi.python.org. This means you can do "easy_install
-notmuch" (or using pip) on your linux box and it will get installed
-into:
-
-/usr/local/lib/python2.x/dist-packages/
-
-For uninstalling, you will need to remove the "notmuch-0.x-py2.x.egg"
-directory and delete one entry refering to cnotmuch in the
-"easy-install.pth" file in that directory. There should be no trace
-left of cnotmuch then.
+The documentation for the latest notmuch release can be `viewed
+online <http://notmuch.readthedocs.org/>`_.
 
 Requirements
 ------------
