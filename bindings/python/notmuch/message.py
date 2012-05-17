notmuch/notmuch

commit 8dc8495010057202b725ac029831c03f4e3ab6bd

python: Fix the remaining broken NULL pointer tests
Signed-off-by: Justus Winter <4winter@informatik.uni-hamburg.de>

bindings/python/notmuch/message.py
@@ -229,7 +229,7 @@ class Message(Python3StringMixIn):
 
         #Returns NULL if any error occurs.
         header = Message._get_header(self._msg, _str(header))
-        if header == None:
+        if not header:
             raise NullPointerError()
         return header.decode('UTF-8', 'ignore')
 
@@ -300,7 +300,7 @@ class Message(Python3StringMixIn):
             raise NotInitializedError()
 
         tags_p = Message._get_tags(self._msg)
-        if tags_p == None:
+        if not tags_p:
             raise NullPointerError()
         return Tags(tags_p, self)
 

bindings/python/notmuch/database.py
@@ -558,7 +558,7 @@ class Database(object):
         """
         self._assert_db_is_initialized()
         tags_p = Database._get_all_tags(self._db)
-        if tags_p == None:
+        if not tags_p:
             raise NullPointerError()
         return Tags(tags_p, self)
 

bindings/python/notmuch/messages.py
@@ -142,7 +142,7 @@ class Messages(object):
         #reset _msgs as we iterated over it and can do so only once
         self._msgs = None
 
-        if tags_p == None:
+        if not tags_p:
             raise NullPointerError()
         return Tags(tags_p, self)
 

bindings/python/notmuch/thread.py
@@ -238,7 +238,7 @@ class Thread(object):
             raise NotInitializedError()
 
         tags_p = Thread._get_tags(self._thread)
-        if tags_p == None:
+        if not tags_p:
             raise NullPointerError()
         return Tags(tags_p, self)
 

commit 8f667be2aca7d903a82344490045dff4110f68d7

python: fix Message.get_header
8dc8495010057202b725ac029831c03f4e3ab6bd introduced a bug, if the
requested header is not set the underlying notmuch function returns an
empty string that also made the expression true resulting in an
exception being raised. Partly revert the commit to fix this
issue. Testing for equality with None is correct in this case since
the restype of the function Message._get_header is c_char_p so NULL
pointers are in fact converted to None in this case.

Signed-off-by: Justus Winter <4winter@informatik.uni-hamburg.de>

bindings/python/notmuch/message.py
@@ -229,7 +229,7 @@ class Message(Python3StringMixIn):
 
         #Returns NULL if any error occurs.
         header = Message._get_header(self._msg, _str(header))
-        if not header:
+        if header == None:
             raise NullPointerError()
         return header.decode('UTF-8', 'ignore')
 