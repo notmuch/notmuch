8fb16e277e4d6c32bafa79ae7967e1e6ba9258e0
Austin Clements <amdragon@MIT.EDU>
2013-12-04 11:19:45 -0500

    lib: Replace NOTMUCH_*_VERSION with LIBNOTMUCH_*_VERSION
    This makes it clear that these macros refer to the *library* version,
    and not to the notmuch application-level release.  Since there are no
    consumers of these macros yet, this is now or never.

--- lib/notmuch.h
+++ lib/notmuch.h
@@ -45,9 +45,9 @@ NOTMUCH_BEGIN_DECLS
  * The library version number.  This must agree with the soname
  * version in Makefile.local.
  */
-#define NOTMUCH_MAJOR_VERSION	3
-#define NOTMUCH_MINOR_VERSION	0
-#define NOTMUCH_MICRO_VERSION	0
+#define LIBNOTMUCH_MAJOR_VERSION	3
+#define LIBNOTMUCH_MINOR_VERSION	0
+#define LIBNOTMUCH_MICRO_VERSION	0
 
 /*
  * Check the version of the notmuch library being compiled against.
@@ -55,19 +55,19 @@ NOTMUCH_BEGIN_DECLS
  * Return true if the library being compiled against is of the
  * specified version or above. For example:
  *
- * #if NOTMUCH_CHECK_VERSION(0, 18, 0)
- *     (code requiring notmuch 0.18 or above)
+ * #if LIBNOTMUCH_CHECK_VERSION(3, 0, 0)
+ *     (code requiring libnotmuch 3.0.0 or above)
  * #endif
  *
- * NOTMUCH_CHECK_VERSION has been defined since version 0.17.0; you
+ * LIBNOTMUCH_CHECK_VERSION has been defined since version 3.0.0; you
  * can use #if !defined(NOTMUCH_CHECK_VERSION) to check for versions
  * prior to that.
  */
-#define NOTMUCH_CHECK_VERSION (major, minor, micro)			\
-    (NOTMUCH_MAJOR_VERSION > (major) ||					\
-     (NOTMUCH_MAJOR_VERSION == (major) && NOTMUCH_MINOR_VERSION > (minor)) || \
-     (NOTMUCH_MAJOR_VERSION == (major) && NOTMUCH_MINOR_VERSION == (minor) && \
-      NOTMUCH_MICRO_VERSION >= (micro)))
+#define LIBNOTMUCH_CHECK_VERSION (major, minor, micro)			\
+    (LIBNOTMUCH_MAJOR_VERSION > (major) ||					\
+     (LIBNOTMUCH_MAJOR_VERSION == (major) && LIBNOTMUCH_MINOR_VERSION > (minor)) || \
+     (LIBNOTMUCH_MAJOR_VERSION == (major) && LIBNOTMUCH_MINOR_VERSION == (minor) && \
+      LIBNOTMUCH_MICRO_VERSION >= (micro)))
 
 typedef int notmuch_bool_t;
 
