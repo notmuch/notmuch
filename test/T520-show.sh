5c526d1737212dd736b1c3fedc6bf5b3493ce436
David Bremner <david@tethera.net>
2014-01-23 08:23:59 -0400

    test: add known broken test exit code of notmuch show
    This test catches a segfault on a syntactically invalid query. It also
    catches a problem with my initial fix, which still returned 0.

---
+++ test/T520-show.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+test_description='"notmuch show"'
+
+. ./test-lib.sh
+
+add_email_corpus
+
+test_begin_subtest "exit code for show invalid query"
+test_subtest_known_broken
+notmuch show foo..
+exit_code=$?
+test_expect_equal 1 $exit_code
+
+test_done
