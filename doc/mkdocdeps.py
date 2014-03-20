57b4ef6f30d41cd14aa570b329841fc2082260ff
David Bremner <david@tethera.net>
2014-03-19 21:48:03 -0300

    doc: fix out-of-tree build
    The subtle part is adding .rst and .py files to vpath so they can be
    used as dependencies without prefixing with $(srcdir)
    
    We also change the interface to mkbuildeps.py: rather than getting the
    containing directory from the conf file path, we go the other way.

--- doc/mkdocdeps.py
+++ doc/mkdocdeps.py
@@ -1,15 +1,16 @@
 from sys import argv
-conffile = argv[1]
+srcdir = argv[1]
 builddir = argv[2]
 outfile = argv[3]
 
-execfile(conffile)
+execfile(srcdir + '/conf.py')
+
 
 roff_files = []
 rst_files = []
 out=open(outfile,'w')
 for page in man_pages:
-    rst_files = rst_files + ["doc/{0:s}.rst".format(page[0])]
+    rst_files = rst_files + ["{0:s}/{1:s}.rst".format(srcdir,page[0])]
     roff_files = roff_files + ["{0:s}/man/{1:s}.{2:d}".format(builddir,page[0],page[4])]
 
 out.write ('MAN_ROFF_FILES := ' + ' \\\n\t'.join(roff_files)+'\n')
