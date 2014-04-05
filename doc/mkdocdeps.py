427b3db243626916d739e2709c5a91212afb5084
W. Trevor King <wking@tremily.us>
2014-04-05 10:31:06 -0700

    doc/mkdocdeps.py: Use "with" statement for the output file
    Before this patch, the open was unnecessarily early and relied on the
    process cleanup to close.  Neither one of these was a real problem,
    but PEP 343's context managers (which landed in Python 2.5) make
    proper cleanup very easy.
    
    [1]: http://legacy.python.org/dev/peps/pep-0343/

--- doc/mkdocdeps.py
+++ doc/mkdocdeps.py
@@ -9,10 +9,10 @@ import conf
 
 roff_files = []
 rst_files = []
-out=open(outfile,'w')
 for page in conf.man_pages:
     rst_files = rst_files + ["{0:s}/{1:s}.rst".format(srcdir,page[0])]
     roff_files = roff_files + ["{0:s}/man/{1:s}.{2:d}".format(builddir,page[0],page[4])]
 
-out.write ('MAN_ROFF_FILES := ' + ' \\\n\t'.join(roff_files)+'\n')
-out.write ('MAN_RST_FILES := ' + ' \\\n\t'.join(rst_files)+'\n')
+with open(outfile, 'w') as out:
+    out.write('MAN_ROFF_FILES := ' + ' \\\n\t'.join(roff_files) + '\n')
+    out.write('MAN_RST_FILES := ' + ' \\\n\t'.join(rst_files) + '\n')
