cbbda62258360f035894cff9dfd66c60b0cc707f
Austin Clements <aclements@csail.mit.edu>
2014-10-03 12:58:03 -0400

    test: Port atomicity test to Python
    Previously, this was implemented using a horrible GDB script (because
    there is no such thing as a non-horrible GDB script).  This GDB script
    often broke with newer versions of GDB for mysterious reasons.  Port
    the test script to GDB's Python API, which makes the code much cleaner
    and, hopefully, more stable.

--- test/T380-atomicity.sh
+++ test/T380-atomicity.sh
@@ -64,7 +64,7 @@ if test_require_external_prereq gdb; then
     # -tty /dev/null works around a conflict between the 'timeout' wrapper
     # and gdb's attempt to control the TTY.
     export MAIL_DIR
-    gdb -tty /dev/null -batch -x $TEST_DIRECTORY/atomicity.gdb notmuch 1>gdb.out 2>&1
+    gdb -tty /dev/null -batch -x $TEST_DIRECTORY/atomicity.py notmuch 1>gdb.out 2>&1
 
     # Get the final, golden output
     notmuch search '*' > expected
