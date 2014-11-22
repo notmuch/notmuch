6c1d626519b04fbc98a5c5497ec4fbcd76a51c00
Jesse Rosenthal <jrosenthal@jhu.edu>
2014-11-22 08:17:15 -0500

    test: Add known-broken test for empty author name
    We test for whether a quoted empty email address
    
        "" <address@example.com>
    
    will show up as the address, instead of the empty string. This is
    marked as known-broken, since the current behavior is to use the empty
    string.
    
    This is a new test file, since handling of unusual email addresses
    doesn't seem to fit well in any of our existing tests.
    
    Signed-off-by: Jesse Rosenthal <jrosenthal@jhu.edu>

---
+++ test/T205-author-naming.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+test_description="naming of authors with unusual addresses"
+. ./test-lib.sh
+
+test_begin_subtest "Add author with empty quoted real name"
+test_subtest_known_broken
+add_message '[subject]="author-naming: Initial thread subject"' \
+	    '[date]="Fri, 05 Jan 2001 15:43:56 -0000"' \
+	    '[from]="\"\" <address@example.com>"'
+output=$(notmuch search --sort=oldest-first author-naming and tag:inbox | notmuch_search_sanitize)
+test_expect_equal "$output" "thread:XXX   2001-01-05 [1/1] address@example.com; author-naming: Initial thread subject (inbox unread)"
+
+test_done
