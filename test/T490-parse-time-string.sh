fd80a9cac1b773d50acef42522ef767c1a2d44b7
Jani Nikula <jani@nikula.org>
2015-03-02 19:47:01 +0200

    test: improve the time parsing tests
    Set the reference time to 12:13:14 instead of 11:11:00 to ensure hours
    and minutes are not mixed up, and seconds are really set instead of
    defaulted to 00.

--- test/T490-parse-time-string.sh
+++ test/T490-parse-time-string.sh
@@ -24,38 +24,38 @@ test_begin_subtest "date(1) --rfc=3339=seconds format"
 test_expect_equal "$(_parse_time 2012-08-03 23:09:37+03:00)" "$(_date 2012-08-03 23:09:37+03:00)"
 
 test_begin_subtest "Date parser tests"
-REFERENCE=$(_date Tue Jan 11 11:11:00 +0000 2011)
+REFERENCE=$(_date Tue Jan 11 12:13:14 +0000 2011)
 cat <<EOF > INPUT
-now          ==> Tue Jan 11 11:11:00 +0000 2011
+now          ==> Tue Jan 11 12:13:14 +0000 2011
 2010-1-1     ==> ERROR: DATEFORMAT
-Jan 2        ==> Sun Jan 02 11:11:00 +0000 2011
-Mon          ==> Mon Jan 10 11:11:00 +0000 2011
+Jan 2        ==> Sun Jan 02 12:13:14 +0000 2011
+Mon          ==> Mon Jan 10 12:13:14 +0000 2011
 last Friday  ==> ERROR: FORMAT
-2 hours ago  ==> Tue Jan 11 09:11:00 +0000 2011
-last month   ==> Sat Dec 11 11:11:00 +0000 2010
-month ago    ==> Sat Dec 11 11:11:00 +0000 2010
-two mo       ==> Thu Nov 11 11:11:00 +0000 2010
-3M           ==> Mon Oct 11 11:11:00 +0000 2010
-4-mont       ==> Sat Sep 11 11:11:00 +0000 2010
-5m           ==> Tue Jan 11 11:06:00 +0000 2011
-dozen mi     ==> Tue Jan 11 10:59:00 +0000 2011
+2 hours ago  ==> Tue Jan 11 10:13:14 +0000 2011
+last month   ==> Sat Dec 11 12:13:14 +0000 2010
+month ago    ==> Sat Dec 11 12:13:14 +0000 2010
+two mo       ==> Thu Nov 11 12:13:14 +0000 2010
+3M           ==> Mon Oct 11 12:13:14 +0000 2010
+4-mont       ==> Sat Sep 11 12:13:14 +0000 2010
+5m           ==> Tue Jan 11 12:08:14 +0000 2011
+dozen mi     ==> Tue Jan 11 12:01:14 +0000 2011
 8am          ==> Tue Jan 11 08:00:00 +0000 2011
 9:15         ==> Tue Jan 11 09:15:00 +0000 2011
 12:34        ==> Tue Jan 11 12:34:00 +0000 2011
-monday       ==> Mon Jan 10 11:11:00 +0000 2011
-yesterday    ==> Mon Jan 10 11:11:00 +0000 2011
+monday       ==> Mon Jan 10 12:13:14 +0000 2011
+yesterday    ==> Mon Jan 10 12:13:14 +0000 2011
 tomorrow     ==> ERROR: KEYWORD
-             ==> Tue Jan 11 11:11:00 +0000 2011 # empty string is reference time
+             ==> Tue Jan 11 12:13:14 +0000 2011 # empty string is reference time
 
 Aug 3 23:06:06 2012             ==> Fri Aug 03 23:06:06 +0000 2012 # date(1) default format without TZ code
 Fri, 03 Aug 2012 23:07:46 +0100 ==> Fri Aug 03 22:07:46 +0000 2012 # rfc-2822
 2012-08-03 23:09:37+03:00       ==> Fri Aug 03 20:09:37 +0000 2012 # rfc-3339 seconds
 
-10s           ==> Tue Jan 11 11:10:50 +0000 2011
-19701223s     ==> Fri May 28 10:37:17 +0000 2010
-19701223      ==> Wed Dec 23 11:11:00 +0000 1970
+10s           ==> Tue Jan 11 12:13:04 +0000 2011
+19701223s     ==> Fri May 28 11:39:31 +0000 2010
+19701223      ==> Wed Dec 23 12:13:14 +0000 1970
 
-19701223 +0100 ==> Wed Dec 23 11:11:00 +0000 1970 # Timezone is ignored without an error
+19701223 +0100 ==> Wed Dec 23 12:13:14 +0000 1970 # Timezone is ignored without an error
 
 today ==^^> Wed Jan 12 00:00:00 +0000 2011
 today ==^> Tue Jan 11 23:59:59 +0000 2011
@@ -65,8 +65,8 @@ this week ==^^> Sun Jan 16 00:00:00 +0000 2011
 this week ==^> Sat Jan 15 23:59:59 +0000 2011
 this week ==_> Sun Jan 09 00:00:00 +0000 2011
 
-two months ago ==> Thu Nov 11 11:11:00 +0000 2010
-two months ==> Thu Nov 11 11:11:00 +0000 2010
+two months ago ==> Thu Nov 11 12:13:14 +0000 2010
+two months ==> Thu Nov 11 12:13:14 +0000 2010
 
 @1348569850 ==> Tue Sep 25 10:44:10 +0000 2012
 @10 ==> Thu Jan 01 00:00:10 +0000 1970
