783a559968968a407cbaa42249613216c27d54f8
David Bremner <david@tethera.net>
2015-05-23 22:28:47 +0200

    test: redirect man output to /dev/null
    In the case the these tests fail, they generate a bunch of output;
    this output is not very interesting because it is just the successful
    output of a man page. It does however make it hard to see what tests are actually failing, even with NOTMUCH_TEST_QUIET

--- test/T010-help-test.sh
+++ test/T010-help-test.sh
@@ -12,9 +12,9 @@ if [ $NOTMUCH_HAVE_MAN -eq 1 ]; then
     test_expect_success 'notmuch help tag' 'notmuch help tag'
 else
     test_expect_success 'notmuch --help tag (man pages not available)' \
-	'test_must_fail notmuch --help tag'
+	'test_must_fail notmuch --help tag >/dev/null'
     test_expect_success 'notmuch help tag (man pages not available)' \
-	'test_must_fail notmuch help tag'
+	'test_must_fail notmuch help tag >/dev/null'
 fi
 
 test_done
