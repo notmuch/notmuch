2982d70ac683c74c62e20081b36529c90bccc914
Mark Walters <markwalters1009@gmail.com>
2015-06-13 09:08:09 +0100

    emacs: add a filter option to show
    Show the current thread with a different filter (i.e., open messages
    in the thread matching the new query).
    
    Bound to 'l' for "limit".
    
    Note that it is not the same as filter in search mode as it replaces
    the existing query rather than ANDing with it (but it does keep the
    thread-id part of the query).

--- emacs/notmuch-show.el
+++ emacs/notmuch-show.el
@@ -47,6 +47,7 @@
 (declare-function notmuch-tree "notmuch-tree"
 		  (&optional query query-context target buffer-name open-target))
 (declare-function notmuch-tree-get-message-properties "notmuch-tree" nil)
+(declare-function notmuch-read-query "notmuch" (prompt))
 
 (defcustom notmuch-message-headers '("Subject" "To" "Cc" "Date")
   "Headers that should be shown in a message, in this order.
@@ -1372,6 +1373,7 @@ reset based on the original query."
     (define-key map (kbd "<backtab>") 'notmuch-show-previous-button)
     (define-key map (kbd "TAB") 'notmuch-show-next-button)
     (define-key map "f" 'notmuch-show-forward-message)
+    (define-key map "l" 'notmuch-show-filter-thread)
     (define-key map "r" 'notmuch-show-reply-sender)
     (define-key map "R" 'notmuch-show-reply)
     (define-key map "|" 'notmuch-show-pipe-message)
@@ -1659,6 +1661,16 @@ user decision and we should not override it."
     (redisplay)
     (save-excursion
       (funcall notmuch-show-mark-read-function (window-start) (window-end)))))
+
+(defun notmuch-show-filter-thread (query)
+  "Filter or LIMIT the current thread based on a new query string.
+
+Reshows the current thread with matches defined by the new query-string."
+  (interactive (list (notmuch-read-query "Filter thread: ")))
+  (let ((msg-id (notmuch-show-get-message-id)))
+    (setq notmuch-show-query-context (if (string= query "") nil query))
+    (notmuch-show-refresh-view t)
+    (notmuch-show-goto-message msg-id)))
 
 ;; Functions for getting attributes of several messages in the current
 ;; thread.
