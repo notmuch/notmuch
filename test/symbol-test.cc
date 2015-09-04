1008fc45da0e206662694eacf12c0678ddcc6162
Jani Nikula <jani@nikula.org>
2015-09-04 21:49:14 +0300

    test: fix whitespace/indentation in symbol-test
    Not of much consequence, but makes it nicer to do further edits.

--- test/symbol-test.cc
+++ test/symbol-test.cc
@@ -3,23 +3,25 @@
 #include <xapian.h>
 #include <notmuch.h>
 
+int main (int argc, char** argv)
+{
+    notmuch_database_t *notmuch;
+    char *message = NULL;
 
-int main(int argc, char** argv) {
-  notmuch_database_t *notmuch;
-  char *message = NULL;
+    if (notmuch_database_open_verbose (argv[1], NOTMUCH_DATABASE_MODE_READ_ONLY,
+				       &notmuch, &message)) {
+	if (message) {
+	    fputs (message, stderr);
+	    free (message);
+	}
+    }
 
-  if (notmuch_database_open_verbose  (argv[1], NOTMUCH_DATABASE_MODE_READ_ONLY, &notmuch, &message))
-      if (message) {
-	  fputs (message, stderr);
-	  free (message);
-      }
+    try {
+	(void) new Xapian::WritableDatabase (argv[2], Xapian::DB_OPEN);
+    } catch (const Xapian::Error &error) {
+	printf("caught %s\n", error.get_msg().c_str());
+	return 0;
+    }
 
-  try {
-    (void) new Xapian::WritableDatabase(argv[2], Xapian::DB_OPEN);
-  } catch (const Xapian::Error &error) {
-    printf("caught %s\n", error.get_msg().c_str());
-    return 0;
-  }
-
-  return 1;
+    return 1;
 }
