notmuch/notmuch

commit e890b0cf4011fd9fd77ebd87343379e4a778888b

database: Store the parent ID for each directory document.
Storing the document ID of the parent of each directory document will
allow us to find all child-directory documents for a given directory
document. We will need this in order to detect directories that have
been removed from the mail store, (though we aren't yet doing this).

lib/database.cc
@@ -84,9 +84,11 @@ typedef struct {
  * maintain data necessary to allow for efficient polling of mail
  * directories.
  *
- * The directory document is indexed with a single prefixed term:
+ * The directory document contains the following terms:
  *
  *	directory:	The directory path (relative to the database path)
+ *	parent:		The document ID of the parent directory document.
+ *			Top-level directories will have a parent value of 0.
  *
  * and has a single value:
  *
@@ -113,6 +115,7 @@ prefix_t BOOLEAN_PREFIX_INTERNAL[] = {
     { "reference", "XREFERENCE" },
     { "replyto", "XREPLYTO" },
     { "directory", "XDIRECTORY" },
+    { "parent", "XPARENT" },
 };
 
 prefix_t BOOLEAN_PREFIX_EXTERNAL[] = {
@@ -587,6 +590,76 @@ directory_db_path (const char *path)
 	return path;
 }
 
+notmuch_status_t
+_find_parent_id (notmuch_database_t *notmuch,
+		 const char *path,
+		 Xapian::docid *parent_id)
+{
+    const char *slash, *parent_db_path;
+    char *parent_path;
+    notmuch_private_status_t private_status;
+    notmuch_status_t status = NOTMUCH_STATUS_SUCCESS;
+
+    if (path == NULL || *path == '\0') {
+	*parent_id = 0;
+	return NOTMUCH_STATUS_SUCCESS;
+    }
+
+    /* Find the last slash (not counting a trailing slash), if any. */
+
+    slash = path + strlen (path) - 1;
+
+    /* First, skip trailing slashes. */
+    while (slash != path) {
+	if (*slash != '/')
+	    break;
+
+	--slash;
+    }
+
+    /* Then, find a slash. */
+    while (slash != path) {
+	if (*slash == '/')
+	    break;
+
+	--slash;
+    }
+
+    /* Finally, skip multiple slashes. */
+    while (slash != path) {
+	if (*slash != '/')
+	    break;
+
+	--slash;
+    }
+
+    if (slash == path)
+	parent_path = talloc_strdup (notmuch, "");
+    else
+	parent_path = talloc_strndup (notmuch, path, slash - path + 1);
+
+    parent_db_path = directory_db_path (parent_path);
+
+    private_status = find_unique_doc_id (notmuch, "directory",
+					 parent_db_path, parent_id);
+    if (private_status == NOTMUCH_PRIVATE_STATUS_NO_DOCUMENT_FOUND) {
+	status = notmuch_database_set_directory_mtime (notmuch,
+						       parent_path, 0);
+	if (status)
+	    return status;
+	private_status = find_unique_doc_id (notmuch, "directory",
+					     parent_db_path, parent_id);
+	status = COERCE_STATUS (private_status, "_find_parent_id");
+    }
+
+    if (parent_db_path != parent_path)
+	free ((char *) parent_db_path);
+
+    talloc_free (parent_path);
+
+    return status;
+}
+
 /* Given a legal 'path' for the database, return the relative path.
  *
  * The return value will be a pointer to the originl path contents,
@@ -632,6 +705,7 @@ notmuch_database_set_directory_mtime (notmuch_database_t *notmuch,
     notmuch_private_status_t status;
     notmuch_status_t ret = NOTMUCH_STATUS_SUCCESS;
     const char *db_path = NULL;
+    Xapian::docid parent_id;
 
     if (notmuch->mode == NOTMUCH_DATABASE_MODE_READ_ONLY) {
 	fprintf (stderr, "Attempted to update a read-only database.\n");
@@ -652,6 +726,16 @@ notmuch_database_set_directory_mtime (notmuch_database_t *notmuch,
 	if (status == NOTMUCH_PRIVATE_STATUS_NO_DOCUMENT_FOUND) {
 	    char *term = talloc_asprintf (NULL, "%s%s",
 					  _find_prefix ("directory"), db_path);
+	    doc.add_term (term);
+	    talloc_free (term);
+
+	    status = _find_parent_id (notmuch, path, &parent_id);
+	    if (status)
+		return status;
+
+	    term = talloc_asprintf (NULL, "%s%u",
+				    _find_prefix ("parent"),
+				    parent_id);
 	    doc.add_term (term);
 	    talloc_free (term);
 

commit a352d9ceaa7e08b7c9de294419ec4c323b81ca15

lib: fix handling of one character long directory names at top level
The code to skip multiple slashes in _notmuch_database_split_path()
skips back one character too much. This is compensated by a +1 in the
length parameter to the strndup() call. Mostly this works fine, but if
the path is to a file under a top level directory with one character
long name, the directory part is mistaken to be part of the file name
(slash == path in code). The returned directory name will be the empty
string and the basename will be the full path, breaking the indexing
logic in notmuch new.

Fix the multiple slash skipping to keep the slash variable pointing at
the last slash, and adjust strndup() accordingly.

The bug was introduced in

commit e890b0cf4011fd9fd77ebd87343379e4a778888b
Author: Carl Worth <cworth@cworth.org>
Date:   Sat Dec 19 13:20:26 2009 -0800

    database: Store the parent ID for each directory document.

just a little over two months after the initial commit in the Notmuch
code history, making this the longest living bug in Notmuch to date.

lib/database.cc
@@ -1781,7 +1781,7 @@ _notmuch_database_split_path (void *ctx,
 
     /* Finally, skip multiple slashes. */
     while (slash != path) {
-	if (*slash != '/')
+	if (*(slash - 1) != '/')
 	    break;
 
 	--slash;
@@ -1794,7 +1794,7 @@ _notmuch_database_split_path (void *ctx,
 	    *basename = path;
     } else {
 	if (directory)
-	    *directory = talloc_strndup (ctx, path, slash - path + 1);
+	    *directory = talloc_strndup (ctx, path, slash - path);
     }
 
     return NOTMUCH_STATUS_SUCCESS;

test/T050-new.sh
@@ -170,7 +170,6 @@ test_expect_equal "$output" "(D) add_files, pass 3: queuing leftover directory $
 No new mail. Removed 3 messages."
 
 test_begin_subtest "One character directory at top level"
-test_subtest_known_broken
 
 generate_message [dir]=A
 generate_message [dir]=A/B
@@ -178,10 +177,6 @@ generate_message [dir]=A/B/C
 
 output=$(NOTMUCH_NEW --debug)
 test_expect_equal "$output" "Added 3 new messages to the database."
-
-# clean up after the broken test to not mess up other tests
-rm -rf "${MAIL_DIR}"/A
-NOTMUCH_NEW 2>&1 > /dev/null
 
 test_begin_subtest "Support single-message mbox"
 cat > "${MAIL_DIR}"/mbox_file1 <<EOF