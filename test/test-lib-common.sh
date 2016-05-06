b183f2635eaf323066f26c7ca22d931d4f4ccd3a
Tomi Ollila <tomi.ollila@iki.fi>
2016-05-06 18:41:57 +0300

    test: copyright information updates
    Files in test directories had only copyright of a single individual,
    of which code was adapted here as a base of the test system.
    Since then many Notmuch Developers have contributed to the test
    system, which is now acknowledged with a constant string in some
    of the test files.
    
    The README file in test directory instructed new files contain a
    copyright notice, but that has never been done (and it is also not
    needed). To simplify things a bit (and lessen confusion) this
    instruction is now removed.
    
    As a side enchangement, all of the 3 entries in the whole source
    tree cd'ing to `dirname` of "$0" now uses syntax cd "$(dirname "$0")".
    This makes these particular lines work when current working directory
    is e.g. /c/Program Files/notmuch/test/.
    (Probably it would fail elsewhere, though.)

--- test/test-lib-common.sh
+++ test/test-lib-common.sh
@@ -1,5 +1,6 @@
 #
 # Copyright (c) 2005 Junio C Hamano
+# Copyright (c) 2010 Notmuch Developers
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
