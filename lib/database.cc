1c3a8e0898be614b5183f0ae626386f7746e7e07
Tomi Ollila <tomi.ollila@iki.fi>
2016-09-27 18:06:52 +0300

    lib/database.cc: fix misleading indentation
    Found by gcc 6.1.1 -Wmisleading-indentation option (set by -Wall).

--- lib/database.cc
+++ lib/database.cc
@@ -1685,8 +1685,8 @@ notmuch_database_begin_atomic (notmuch_database_t *notmuch)
 	notmuch->atomic_nesting > 0)
 	goto DONE;
 
-	if (notmuch_database_needs_upgrade(notmuch))
-		return NOTMUCH_STATUS_UPGRADE_REQUIRED;
+    if (notmuch_database_needs_upgrade (notmuch))
+	return NOTMUCH_STATUS_UPGRADE_REQUIRED;
 
     try {
 	(static_cast <Xapian::WritableDatabase *> (notmuch->xapian_db))->begin_transaction (false);
