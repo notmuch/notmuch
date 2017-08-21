39cc84e9fc84065835bce0b0ebd92f4c7adbb23a
Yuri Volchkov <yuri.volchkov@gmail.com>
2017-08-21 17:44:47 +0200

    test: show id:<> works even if the first duplicate is deleted
    Signed-off-by: Yuri Volchkov <yuri.volchkov@gmail.com>

--- test/T670-duplicate-mid.sh
+++ test/T670-duplicate-mid.sh
@@ -37,4 +37,30 @@ notmuch reindex '*'
 notmuch search --output=files "sekrit" | notmuch_dir_sanitize > OUTPUT
 test_expect_equal_file EXPECTED OUTPUT
 
+rm ${MAIL_DIR}/copy1
+test_begin_subtest 'Deleted first duplicate file does not stop notmuch show from working'
+test_subtest_known_broken
+output=$(notmuch show --body=false --format=json id:duplicate)
+expected='[[[{
+    "id": "'duplicate'",
+    "match": true,
+    "excluded": false,
+    "filename": [
+        "'"${MAIL_DIR}"/copy1'",
+        "'"${MAIL_DIR}"/copy2'"
+    ],
+    "timestamp": 978709435,
+    "date_relative": "2001-01-05",
+    "tags": ["inbox","unread"],
+    "headers": {
+        "Subject": "message 2",
+        "From": "Notmuch Test Suite <test_suite@notmuchmail.org>",
+        "To": "Notmuch Test Suite <test_suite@notmuchmail.org>",
+        "Date": "Fri, 05 Jan 2001 15:43:55 +0000"
+    }
+ },
+[]]]]'
+
+test_expect_equal_json "$output" "$expected"
+
 test_done
