4ac23c5978ff963c18a012b6b230b26f9ef98c43
David Bremner <david@tethera.net>
2017-12-14 10:32:34 -0400

    test: add known broken test for regexp search of second subject
    We expect this to give the same answer as the non-regexp subject
    search. It does not because the regexp search relies on the value
    slot, which currently contains only one subject.

--- test/T670-duplicate-mid.sh
+++ test/T670-duplicate-mid.sh
@@ -47,6 +47,16 @@ EOF
 notmuch search --output=files subject:'"message 2"' | notmuch_dir_sanitize > OUTPUT
 test_expect_equal_file EXPECTED OUTPUT
 
+test_begin_subtest 'Regexp search for second subject'
+test_subtest_known_broken
+cat <<EOF >EXPECTED
+MAIL_DIR/copy0
+MAIL_DIR/copy1
+MAIL_DIR/copy2
+EOF
+notmuch search --output=files 'subject:"/message 2/"' | notmuch_dir_sanitize > OUTPUT
+test_expect_equal_file EXPECTED OUTPUT
+
 add_message '[id]="duplicate"' '[body]="sekrit" [filename]=copy3'
 test_begin_subtest 'search for body in duplicate file'
 cat <<EOF >EXPECTED
