c846e15ffee526b70a28b512710968b2db77b724
David Bremner <david@tethera.net>
2018-09-09 22:36:17 -0300

    test: make regexp test conditional on field processors
    Normally we'd mark it broken, but perversely missing regexp support
    actually makes the test pass.

--- test/T670-duplicate-mid.sh
+++ test/T670-duplicate-mid.sh
@@ -48,7 +48,11 @@ notmuch search --output=files subject:'"message 2"' | notmuch_dir_sanitize > OUT
 test_expect_equal_file EXPECTED OUTPUT
 
 test_begin_subtest 'Regexp search for second subject'
-test_subtest_known_broken
+# Note that missing field processor support really means the test
+# doesn't make sense, but it happens to pass.
+if [ $NOTMUCH_HAVE_XAPIAN_FIELD_PROCESSOR -eq 1 ]; then
+    test_subtest_known_broken
+fi
 cat <<EOF >EXPECTED
 MAIL_DIR/copy0
 MAIL_DIR/copy1
