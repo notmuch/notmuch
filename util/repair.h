ff3d873f0b80b023764fe9d3fc0609b81d82775e
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2019-08-29 11:38:51 -0400

    util/repair: add _notmuch_repair_crypto_payload_skip_legacy_display
    This is a utility function designed to make it easier to
    "fast-forward" past a legacy-display part associated with a
    cryptographic envelope, and show the user the intended message body.
    
    The bulk of the ugliness in here is in the test function
    _notmuch_crypto_payload_has_legacy_display, which tests all of the
    things we'd expect to be true in a a cryptographic payload that
    contains a legacy display part.
    
    Signed-off-by: Daniel Kahn Gillmor <dkg@fifthhorseman.net>

--- util/repair.h
+++ util/repair.h
@@ -11,6 +11,23 @@ extern "C" {
  * techniques that are designed to improve the user experience of
  * notmuch */
 
+/* If payload is a cryptographic payload within an encrypted message, and
+ * it has a "legacy display" part, then we can skip over it and jump
+ * to the actual content, because notmuch already handles protected
+ * headers appropriately.
+ *
+ * This function either returns payload directly (if it does not have
+ * a "legacy display" part), or it returns a pointer to its
+ * content-bearing subpart, with the "legacy display" part and the
+ * surrounding multipart/mixed object bypassed.
+ *
+ * No new objects are created by calling this function, and the
+ * returned object will only be released when the original part is
+ * disposed of.
+ */
+GMimeObject *
+_notmuch_repair_crypto_payload_skip_legacy_display (GMimeObject *payload);
+
 #ifdef __cplusplus
 }
 #endif
