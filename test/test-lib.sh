7c7cebffe6f05c8bbb07c3714fde08562444c72e
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2020-04-30 15:35:21 -0400

    tests/smime: include secret key material for Bob
    This is taken from the same Internet Draft that test/smime/ca.crt
    comes from.  See that draft for more details.
    https://www.ietf.org/id/draft-dkg-lamps-samples-02.html#name-pkcs12-object-for-bob
    
    We don't use it yet, but it will be used to decrypt other messages in
    the test suite.
    
    Note that we include it here with an empty passphrase, rather than
    with the passphrase "bob" that it is supplied with in the I-D.  The
    underlying cryptographic material is the same, but this way we can
    import cleanly into gpgsm without having a passphrase set on it (gpgsm
    converts an empty-string passphrase into no passphrase at all on
    import).
    
    Signed-off-by: Daniel Kahn Gillmor <dkg@fifthhorseman.net>

--- test/test-lib.sh
+++ test/test-lib.sh
@@ -145,6 +145,8 @@ add_gpgsm_home ()
     gpgsm --quiet --batch --no-tty --no-common-certs-import --disable-dirmngr --import < $NOTMUCH_SRCDIR/test/smime/ca.crt
     echo "4D:E0:FF:63:C0:E9:EC:01:29:11:C8:7A:EE:DA:3A:9A:7F:6E:C1:0D S" >> "$GNUPGHOME/trustlist.txt"
     echo include-certs::1 | gpgconf --output /dev/null --change-options gpgsm
+    gpgsm --batch --no-tty --no-common-certs-import --pinentry-mode=loopback --passphrase-fd 3 \
+              --disable-dirmngr --import "$NOTMUCH_SRCDIR/test/smime/bob.p12" >>"$GNUPGHOME"/import.log 2>&1 3<<<''
     test_debug "cat $GNUPGHOME/import.log"
 }
 
