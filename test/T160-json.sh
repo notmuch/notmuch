b46d842782527b206e139edd00ab1ac896b5a23b
David Bremner <david@tethera.net>
2020-06-24 11:32:34 -0300

    test: mark two tests broken on machines with 32 bit time_t
    I haven't traced the code path as exhaustively for the SMIME test, but
    the expiry date in question is larger then representable in a signed
    32 bit integer.

--- test/T160-json.sh
+++ test/T160-json.sh
@@ -65,6 +65,9 @@ test_expect_equal_json "$output" "[{\"thread\": \"XXX\",
  \"unread\"]}]"
 
 test_begin_subtest "Search message: json, 64-bit timestamp"
+if [ $NOTMUCH_HAVE_64BIT_TIME_T -ne 1 ]; then
+    test_subtest_known_broken
+fi
 add_message "[subject]=\"json-search-64bit-timestamp-subject\"" "[date]=\"Tue, 01 Jan 2999 12:00:00 -0000\"" "[body]=\"json-search-64bit-timestamp-message\""
 output=$(notmuch search --format=json "json-search-64bit-timestamp-message" | notmuch_search_sanitize)
 test_expect_equal_json "$output" "[{\"thread\": \"XXX\",
