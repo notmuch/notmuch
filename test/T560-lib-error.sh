46e8076281073c8df5ae8e7e5b386981e2ef4c62
David Bremner <david@tethera.net>
2020-07-04 09:16:30 -0300

    lib: add known broken test for notmuch_message_get_filename
    This will be fixed in the next commit

--- test/T560-lib-error.sh
+++ test/T560-lib-error.sh
@@ -405,4 +405,21 @@ cat <<EOF > EXPECTED
 EOF
 test_expect_equal_file EXPECTED OUTPUT
 
+test_begin_subtest "Handle getting message filename from closed database"
+test_subtest_known_broken
+cat c_head2 - c_tail <<'EOF' | test_C ${MAIL_DIR}
+    {
+        const char *filename;
+        filename = notmuch_message_get_filename (message);
+        printf("%d\n%d\n", message != NULL, filename == NULL);
+    }
+EOF
+cat <<EOF > EXPECTED
+== stdout ==
+1
+1
+== stderr ==
+EOF
+test_expect_equal_file EXPECTED OUTPUT
+
 test_done
