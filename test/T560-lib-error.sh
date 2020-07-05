33dd5fdc6997faee43aa0f79693fc4a8b5143756
David Bremner <david@tethera.net>
2020-07-05 10:00:23 -0300

    lib: catch Xapian exceptions in n_m_add_tag
    This is mostly just (horizontal) code movement due to wrapping
    everything in a try / catch.

--- test/T560-lib-error.sh
+++ test/T560-lib-error.sh
@@ -503,7 +503,6 @@ EOF
 test_expect_equal_file EXPECTED OUTPUT
 
 test_begin_subtest "Handle adding tag with closed database"
-test_subtest_known_broken
 cat c_head2 - c_tail <<'EOF' | test_C ${MAIL_DIR}
     {
         notmuch_status_t status;
