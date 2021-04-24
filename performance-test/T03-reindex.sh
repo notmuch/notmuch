8cb30b08d8c9a650b8b5a98f4547b1b48e3f9d03
David Bremner <david@tethera.net>
2021-04-24 14:46:18 -0300

    perf-test: add test for reindexing after removal/renaming msgs
    At this point it is a bit tricky to measure the performance increase
    from the new message deletion code, since the same commit (9ad19e4)
    that improved the performance also seems to have fixed a bug with an
    uncaught Xapian exception triggered by this test.

--- performance-test/T03-reindex.sh
+++ performance-test/T03-reindex.sh
@@ -10,4 +10,32 @@ time_run 'reindex *' "notmuch reindex '*'"
 time_run 'reindex *' "notmuch reindex '*'"
 time_run 'reindex *' "notmuch reindex '*'"
 
+manifest=$(mktemp manifestXXXXXX)
+
+find mail -type f ! -path 'mail/.notmuch/*' | sed -n '1~4 p' > $manifest
+# arithmetic context is to eat extra whitespace on e.g. some BSDs
+count=$((`wc -l < $manifest`))
+
+xargs tar uf backup.tar < $manifest
+
+perl -nle 'rename $_, "$_.renamed"' $manifest
+
+time_run "reindex ($count mv)" "notmuch reindex '*'"
+
+perl -nle 'rename "$_.renamed", $_' $manifest
+
+time_run "reindex ($count mv back)" "notmuch reindex '*'"
+
+perl -nle 'unlink $_; unlink $_.copy' $manifest
+
+time_run "reindex ($count rm)" "notmuch reindex '*'"
+
+tar xf backup.tar
+
+time_run "reindex ($count restore)" "notmuch reindex '*'"
+
+perl -nle 'link $_, "$_.copy"' $manifest
+
+time_run "reindex ($count cp)" "notmuch reindex '*'"
+
 time_done
