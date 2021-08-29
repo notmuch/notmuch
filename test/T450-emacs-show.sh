9fe36d7dc79df93e75de44290faf3fed66e688c0
David Bremner <david@tethera.net>
2021-08-29 12:23:33 -0700

    test/emacs: test for notmuch-show with nonexistent CWD
    Somewhat predictably, the other code path in
    notmuch-call-notmuch--helper also needs to be fixed.

--- test/T450-emacs-show.sh
+++ test/T450-emacs-show.sh
@@ -236,4 +236,12 @@ test_emacs '(let ((notmuch-crypto-process-mime nil))
              (test-visible-output))'
 test_expect_equal_file $EXPECTED/notmuch-show-decrypted-message-no-crypto OUTPUT
 
+test_begin_subtest "notmuch-show with nonexistent CWD"
+tid=$(notmuch search --limit=1 --output=threads '*' | sed s/thread://)
+test_subtest_known_broken
+test_emacs "(test-log-error
+	      (let ((default-directory \"/nonexistent\"))
+	        (notmuch-show \"$tid\")))"
+test_expect_equal "$(cat MESSAGES)" "COMPLETE"
+
 test_done
