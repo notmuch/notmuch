371fa28ced91e0d0036a98403fab61e9f5301ba4
David Bremner <david@tethera.net>
2022-02-12 16:27:33 -0400

    test: split variable settings to their own file
    This allows sharing more variable settings between the (correctness)
    tests and the performance-tests. Unfortunately it seems a bit tricky
    to move settings to test-lib-common.sh, as that is sourced late in
    test-lib.sh, and moving it earlier breaks things.

--- performance-test/perf-test-lib.sh
+++ performance-test/perf-test-lib.sh
@@ -41,6 +41,8 @@ done
 # Ensure NOTMUCH_SRCDIR and NOTMUCH_BUILDDIR are set.
 . $(dirname "$0")/../test/export-dirs.sh || exit 1
 
+. "$NOTMUCH_SRCDIR/test/test-vars.sh" || exit 1
+
 # Where to run the tests
 TEST_DIRECTORY=$NOTMUCH_BUILDDIR/performance-test
 
