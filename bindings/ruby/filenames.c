837426d7be63cf11d51b74464f174724935c5d4f
Felipe Contreras <felipe.contreras@gmail.com>
2023-03-27 15:59:41 -0600

    ruby: filenames: return string array directly
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- bindings/ruby/filenames.c
+++ bindings/ruby/filenames.c
@@ -23,7 +23,11 @@
 VALUE
 notmuch_rb_filenames_get (notmuch_filenames_t *fnames)
 {
-    return Data_Wrap_Notmuch_Object (notmuch_rb_cFileNames, &notmuch_rb_filenames_type, fnames);
+    VALUE rb_array = rb_ary_new ();
+
+    for (; notmuch_filenames_valid (fnames); notmuch_filenames_move_to_next (fnames))
+	rb_ary_push (rb_array, rb_str_new2 (notmuch_filenames_get (fnames)));
+    return rb_array;
 }
 
 /*
