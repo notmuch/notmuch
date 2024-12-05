969b26704da11c9e1935e5b442f6a88b32bb7e22
Fredrik Lönnegren <fredrik@frelon.se>
2024-12-05 17:45:50 +0100

    docs: Update intersphinx_mapping
    Running `make dirhtml` in bindings/python/docs generates the following
    error:
    
    ```
    sphinx-build -b dirhtml -d build/doctrees   source build/dirhtml
    Running Sphinx v8.0.2
    loading translations [en]... done
    making output directory... done
    Converting `source_suffix = '.rst'` to `source_suffix = {'.rst': 'restructuredtext'}`.
    ERROR: Invalid value `None` in intersphinx_mapping['https://docs.python.org/']. Expected a two-element tuple or list.
    ```
    
    This commit updates intersphinx_mapping to follow the latest docs [1]
    
    [1] https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
    
    Signed-off-by: Fredrik Lönnegren <fredrik@frelon.se>

--- bindings/python/docs/source/conf.py
+++ bindings/python/docs/source/conf.py
@@ -206,4 +206,4 @@ latex_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
